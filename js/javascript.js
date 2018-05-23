ad6f0eb3b2ae624bc3ec421590657175dc9cb01a
ashwin goel <goelashwin36@gmail.com>
2018-05-23 16:48:58 +0530

    Some changes

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+// Get the navbar
+var navbar = document.getElementById("navbar");
+var home=document.getElementById("home").offsetTop;
+var my_projects=document.getElementById("my_projects");
+var about_me=document.getElementById("about_me");
+var gallery=document.getElementById("gallery");
+var contact=document.getElementById("contact");
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >=contact.pageYOffset) {
+        navbar.classList.add("stickyactive")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
